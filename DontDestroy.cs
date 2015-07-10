9804812740f6c20392679b552d218afe437c1fd3
namlunoy <hoangvancong123@gmail.com>
2015-07-10 09:22:06 +0700

    Script: Dont destroy game object

---
+++ DontDestroy.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DontDestroy<T> :  MonoBehaviour where T : MonoBehaviour {
+    
+    private static T _instance;
+    
+    void Awake()
+    {
+        if(_instance == null)
+        {
+            _instance = this.GetComponent<T>();
+            DontDestroyOnLoad(_instance.gameObject);
+        }
+        else
+        {
+            if (this != _instance)
+                Destroy(this.gameObject);
+        }
+    }
+
+}
